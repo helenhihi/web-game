7ceb1dcda41c3c9ea9447bfd1bf8e2b3667ac07b
helenhihi <hrparka@naver.com>
2022-01-27 18:47:26 +0900

    Update index.js

--- number-baseball/index.js
+++ number-baseball/index.js
@@ -15,7 +15,6 @@ for (let n = 0; n < 4; n++) { // 네 번 반복
 console.log(answer);
 
 const tries = [];
-
 function checkInput(input) {
   if (input.length !== 4) { // 길이는 4가 아닌가
     return alert('4자리 숫자를 입력해 주세요.');
@@ -66,9 +65,9 @@ $form.addEventListener('submit', (event) => {
   }
   if (strike === 0 && ball === 0) {
     out++;
-    $logs.append(`${value} : 아웃`, document.createElement('br'));
+    $logs.append(`${value}:아웃`, document.createElement('br'));
   } else {
-    $logs.append(`${value} : ${strike}스트라이크 ${ball}볼`, document.createElement('br'));
+    $logs.append(`${value}:${strike} 스트라이크 ${ball} 볼`, document.createElement('br'));
   }
   if (out === 3) {
     defeated();
