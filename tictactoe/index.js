7bd39f7d2b5a5d9c873f90e81b00ffd3cb24b4f5
helenhihi <hrparka@naver.com>
2022-02-18 17:52:50 +0900

    Update index.js

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -1,45 +1,61 @@
-/* <table>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    </table> */
+/*  <table>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>  */
 
-let turn = "O";
-const data = [];
-for (let i = 0; i < 3; i++) {
-  data.push([]);
-}
+const { body } = document;
+
 const table = document.createElement("table");
-for (let i = 0; i < 3; i++) {
+const result = document.createElement("div");
+const rows = [];
+let turn = "O";
+
+  // [
+  //   [td, td, td],
+  //   [td, td, td],
+  //   [td, td, td],
+  // ]
+
+const callback = (event) => {
+  if (event.target.innerText !== "") { // 칸이 이미 채워져 있는가?
+    console.log("빈칸이 아닙니다.");
+    return;
+  }
+  // 빈칸이면
+  console.log("빈칸입니다");
+  event.target.innerText = turn;
+
+  if (turn === "O") {
+    turn = "X";
+  } else if (turn === "X") {
+    turn = "O";
+  }
+};
+
+for (let i = 1; i <= 3; i++) {
   const tr = document.createElement("tr");
-  for (let j = 0; j < 3; j++) {
+  const cells = [];
+  for (let j = 1; j <= 3; j++) {
     const td = document.createElement("td");
-    td.addEventListener("click", (event) => {
-      // 칸에 글자가 있나?
-      if (event.target.innerText) return;
-      console.log("clicked");
-      event.target.innerText = turn;
-      // 승부 확인
-      if (turn === "O") {
-        turn = "X";
-      } else if (turn ==="X") {
-        turn = "O";
-      }
-    });
+    cells.push("td");
+    td.addEventListener("click", callback);
     tr.append(td);
   }
+  rows.push(cells);
   table.append(tr);
 }
-document.body.append(table);
+body.append(table);
+body.append(result);
